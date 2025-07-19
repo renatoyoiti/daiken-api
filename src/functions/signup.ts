d1334dff19344acf291fc441eeac02c478b4b937
Renato Yoiti <renatoyoiti@gmail.com>
2025-07-19 20:13:32 -0300

    Estrutura do projeto

---
+++ src/functions/signup.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { SignUpController } from "../controllers/SignUpController";
+import { parseEvent } from '../utils/parseEvent';
+import { parseResponse } from '../utils/parseResponse';
+
+export async function handle(event: APIGatewayProxyEventV2) {
+  const request = parseEvent(event);
+  const response = await SignUpController.handle(request);
+  return parseResponse(response);
+}
--- src/signin.ts
+++
@@ -1,8 +0,0 @@
-export async function handle(event) {
-    return {
-        statusCode: event,
-        body: JSON.stringify({
-            hello: 'Hello World'
-        })
-    }
-}
